6f8c764eceb8a52f854b525c4b12cfd2e82bc198
Gael Borchardt <gaelborchardt@gmail.com>
2026-03-08 15:02:44 -0700

    fix:cors

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,15 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
 
   const port = process.env.PORT || 3000;
   await app.listen(port, '0.0.0.0');
   console.log(`App running on port ${port}`);
-
-  app.enableCors({
-    origin: '*', // o un array de URLs específicas
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
 }
 bootstrap();
